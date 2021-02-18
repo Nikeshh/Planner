0570ad01338c7b37eb15b9c3528e9b3bc4c4635a
nikeshh <nikeshhbaskaran01@gmail.com>
2021-02-18 20:09:05 +0530

    Defined the necessary theme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
+import { BrowserRouter } from "react-router-dom";
+import theme from "./theme";
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <BrowserRouter>
+          <MuiThemeProvider theme={theme}>
+
+          </MuiThemeProvider>
+        </BrowserRouter>
       </div>
     );
   }
