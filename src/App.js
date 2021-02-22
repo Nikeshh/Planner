29896d4bf493f7fd2a7499f762fd03c1b10833c8
nikeshh <nikeshhbaskaran01@gmail.com>
2021-02-22 16:57:38 +0530

    Added global configurations

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React, { Component } from "react";
+import React, { Fragment, Suspense, lazy, Component, lazy } from "react";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import theme from "./theme";
+import GlobalStyles from "./GlobalStyles";
+import Pace from "./shared/components/Pace";
+
+const LoggedInComponent = lazy(() => import("./logged_in/components/Main"));
+
+const LoggedOutComponent = lazy(() => import("./logged_out/components/Main"));
 
 class App extends Component {
   render() {
@@ -9,7 +15,23 @@ class App extends Component {
       <div>
         <BrowserRouter>
           <MuiThemeProvider theme={theme}>
-
+            <CssBaseline />
+            <GlobalStyles />
+            <Pace color={theme.palette.primary.light} />
+            {/*
+              Why is suspense used here?
+              What is the use of Fragment?
+             */}
+            <Suspense fallback={<Fragment />}>
+                <Switch>
+                  <Route path="/c">
+                    <LoggedInComponent />
+                  </Route>
+                  <Route>
+                    <LoggedOutComponent />
+                  </Route>
+                </Switch>
+            </Suspense>
           </MuiThemeProvider>
         </BrowserRouter>
       </div>
